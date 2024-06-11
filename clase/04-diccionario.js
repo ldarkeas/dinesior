8380ae19c3ffe7c9aabab7e682a4a23399376260
ldarkeas <leas2002fi@gmail.com>
2024-06-10 20:52:32 -0600

    10/6

---
+++ clase/04-diccionario.js
@@ -0,0 +1,12 @@
+const mascota = {
+    nombre : 'apolo',
+    edad : 2
+}
+mascota.raza = 'terrier'
+mascota.edad = 4
+
+console.log(mascota.nombre)
+console.log(mascota.edad)
+console.log(mascota.raza)
+
+
