ded5a4230174899a94438db64f086aeefdaf18d5
ldarkeas <leas2002fi@gmail.com>
2024-06-12 20:08:19 -0600

    11/6

--- clase/04-diccionario.js
+++ clase/04-diccionario.js
@@ -1,12 +1,74 @@
+
+
+// * clave y un valor
+
+// objeto / diccionario / mapa
 const mascota = {
-    nombre : 'apolo',
-    edad : 2
+    nombre: 'Apolo',
+    edad: 2,
+    comidas_favoritas: ['carne', 'pollo', 'pescado'],
+    peso: [
+        {
+            unidad: 'kg',
+            valor: 5
+        },
+        {
+            unidad: 'lb',
+            valor: 11 // ⬅️
+        }
+    ]
 }
-mascota.raza = 'terrier'
-mascota.edad = 4
 
-console.log(mascota.nombre)
-console.log(mascota.edad)
-console.log(mascota.raza)
+// console.log(mascota.peso[1].valor)
 
+mascota.raza = 'Terrier'
+
+mascota.edad = 3
+
+delete mascota.edad // elimina la propiedad/clave
+
+// console.log(mascota.nombre)
+// console.log(mascota.edad) // undefined
+// console.log(mascota)
+
+
+const usuario = [
+    {
+        nombre: 'Juan',
+        edad: 25,
+        email: '',
+        address: { // []
+            calle: 'Av. Siempre Viva',
+            numero: 123,
+            geo: {
+                lat: 123.123,// ⬅️
+                lng: 321.321
+            }
+        }
+    },
+    {
+        nombre: 'Pedro',
+        edad: 25,
+        email: '',
+        address: { // []
+            calle: 'Av. Siempre Viva',
+            numero: 123,
+            geo: {
+                lat: 123.123,// ⬅️
+                lng: 321.321
+            }
+        }
+    },
+]
+
+// * usuario        => arreglo
+// * usuario[0]     => diccionario
+
+console.log(usuario)
+// console.log(usuario[0].nombre)
+
+// * item => diccionario
+for (const item of usuario) {
+    console.log(item.nombre)
+}
 
